b33dc7e4ff0d2131d86b958c003290384919af19
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2021-11-21 12:23:09 +0200

    Some changes

--- docs/src/routes/index.svelte
+++ docs/src/routes/index.svelte
@@ -3,12 +3,45 @@
 </script>
 
 <script lang="ts">
-	import { Button } from 'fluent-svelte';
+	import { Button, Expander, ContentDialog, TextBlock, ComboBox, Checkbox, TextBox } from 'fluent-svelte';
 	import 'fluent-svelte/theme.css';
+
+	let dialogOpen = false;
+	let dialogResult = "";
 </script>
 
-<div class="container">
-	<h1 style="padding: 10px;">Hello world</h1>
-	<Button style="margin-left:10px;" variant="accent">fff</Button>
-	<Button variant="hyperlink">fff</Button>
-</div>
+<h2>Hello!</h2>
+<TextBlock style="margin-top: 10px;margin-bottom:10px;">This is a work in progress site and you shouldn't visit this yet!</TextBlock>
+<br>
+<Button on:click={() => window.history.back()}>Go back to your place</Button>
+
+<Expander style="margin-top:10px;">
+	Title
+	<svelte:fragment slot="content">
+		Test Expander<br>
+		<Button style="margin-top:10px;">And a button!</Button>
+
+		<Button on:click={() => (dialogOpen = true)} style="margin-top:10px">Open to see something</Button>
+	<ContentDialog bind:open={dialogOpen} title="Content Dialog">
+		<div style="display: flex; flex-direction: column; gap: 3px; margin: 0;">
+			<TextBlock>Wow</TextBlock>
+			<Checkbox>Test</Checkbox>
+		</div>
+		<svelte:fragment slot="footer">
+			<Button
+				variant="accent"
+				on:click={() => {
+					dialogOpen = false;
+					dialogResult = "Do the thing!";
+				}}>Do the thing!</Button
+			>
+			<Button
+				on:click={() => {
+					dialogOpen = false;
+					dialogResult = "Cancel";
+				}}>Cancel</Button
+			>
+		</svelte:fragment>
+	</ContentDialog>
+	</svelte:fragment>
+</Expander>
